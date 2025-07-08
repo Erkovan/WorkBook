2391bc78b6eee0f7a5132d989dff1a2089b97c13
Oğuzalp Erkovan <oguzalp.erkovan@gmail.com>
2025-07-08 19:19:58 +0300

    Create firstpython.py
    Just a test

---
+++ firstpython.py
@@ -0,0 +1,8 @@
+list = [1,2,3,4,5,6,6,7,8,9,9,87,75,675,6,75,6756,7]
+
+
+for i in list:
+   if i>500:
+       print(str(i) + "is a above 500!"
+   else: print( str(i) + " is not greater than 500")
+    
